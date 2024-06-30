8904cf5623a2cdc235638ea8611917f1b83a5d27
◄👨‍💻𝑴𝑼𝑹𝑨𝑳𝑰 ✽ 𝑩𝑶𝑻𝒁 ꨄ︎ <148250637+MuraliBotz@users.noreply.github.com>
2024-06-30 11:12:18 +0530

    Create __init__.py

---
+++ AiChatBot/__init__.py
@@ -0,0 +1,61 @@
+import asyncio
+import logging
+import time
+from importlib import import_module
+from os import listdir, path
+from dotenv import load_dotenv
+import config
+import pymongo
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID
+from pyrogram.enums import ParseMode, ChatMemberStatus
+from Abg import patch
+
+loop = asyncio.get_event_loop()
+load_dotenv()
+boot = time.time()
+
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.INFO,
+)
+
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("pymongo").setLevel(logging.ERROR)
+
+
+
+class ChikuBot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Chikubot",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            lang_code="en",
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.DEFAULT,
+        )
+
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
+
+    async def stop(self):
+        await super().stop()
+        
+            
+
+
+Chiku = ChikuBot()
+
+
+
+#from AiChatBot.Db import git
+
+#Up = git()
+
