02a7aca528178b33b1e78eaa6d919c435fdd4109
◄👨‍💻𝑴𝑼𝑹𝑨𝑳𝑰 ✽ 𝑩𝑶𝑻𝒁 ꨄ︎ <148250637+MuraliBotz@users.noreply.github.com>
2024-07-01 11:55:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "6844821478").split())
+    map(int, getenv("OWNER_ID", "6764358144").split())
 ) 
 
 #Fill Only Username Without @
