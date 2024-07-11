c9edc281f3d8330c2f4ca5d2762a2cb546423e15
◄👨‍💻𝑴𝑼𝑹𝑨𝑳𝑰 ✽ 𝑩𝑶𝑻𝒁 ꨄ︎ <148250637+MuraliBotz@users.noreply.github.com>
2024-07-12 00:42:50 +0530

    Update Telegraph.py

--- AiChatBot/modules/Telegraph.py
+++ AiChatBot/modules/Telegraph.py
@@ -1,28 +1,38 @@
 import os
-
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from telegraph import upload_file
-
 from AiChatBot import Chiku
 
+#credit - Vivek Kumar 
 
-@Chiku.on_cmd(["Tgm", "tl"])
-async def telegraph(client, message):
+@Chiku.on_cmd(["tgm", "tgt", "telegraph", "tl"])
+async def get_link_group(client, message):
     if not message.reply_to_message:
         return await message.reply_text(
-            "ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇᴅɪᴀ ᴛᴏ ᴜᴘʟᴏᴀᴅ ᴏɴ ᴛᴇʟᴇɢʀᴀᴘʜ"
+            "Pʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇᴅɪᴀ ᴛᴏ ᴜᴘʟᴏᴀᴅ ᴏɴ Tᴇʟᴇɢʀᴀᴘʜ"
         )
+
+    media = message.reply_to_message
+    file_size = 0
+    if media.photo:
+        file_size = media.photo.file_size
+    elif media.video:
+        file_size = media.video.file_size
+    elif media.document:
+        file_size = media.document.file_size
+
+    if file_size > 5 * 1024 * 1024:
+        return await message.reply_text("Pʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴍᴇᴅɪᴀ ғɪʟᴇ ᴜɴᴅᴇʀ 𝟻MB.")
     try:
-        text = await message.reply("ᴘʀᴏᴄᴇssɪɴɢ...")
+        text = await message.reply("Pʀᴏᴄᴇssɪɴɢ...")
 
         async def progress(current, total):
-            await text.edit_text(f"ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ... {current * 100 / total:.1f}%")
+            await text.edit_text(f"📥 Dᴏᴡɴʟᴏᴀᴅɪɴɢ... {current * 100 / total:.1f}%")
 
         try:
-            local_path = await message.reply_to_message.download( progress=progress
-            )
-            await text.edit_text(" ᴜᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ...")
+            local_path = await media.download(progress=progress)
+            await text.edit_text("📤Uᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ...")
             upload_path = upload_file(local_path)
             await text.edit_text(
                 f"🌐 | [ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ](https://telegra.ph{upload_path[0]})",
@@ -40,13 +50,13 @@ async def telegraph(client, message):
             try:
                 os.remove(local_path)
             except Exception:
-               pass
+                pass
         except Exception as e:
-            await text.edit_text(f"❌ |ғɪʟᴇ ᴜᴘʟᴏᴀᴅ ғᴀɪʟᴇᴅ \n\n<i>ʀᴇᴀsᴏɴ: {e}</i>")
+            await text.edit_text(f"❌ Fɪʟᴇ ᴜᴘʟᴏᴀᴅ ғᴀɪʟᴇᴅ\n\n<i>Rᴇᴀsᴏɴ: {e}</i>")
             try:
                 os.remove(local_path)
             except Exception:
-               pass
+                pass
             return
     except Exception:
         pass
